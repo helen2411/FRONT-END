f545d2f2e5a92ac7b0a13fc06c1f1c5bca7d2b2a
helen2411 <helen.l.gomes@aluno.senai.br>
2024-11-22 17:27:34 -0300

    E-books

--- JavaScript/aula4-while/scripts/script.js
+++ JavaScript/aula4-while/scripts/script.js
@@ -16,21 +16,51 @@ function testes(){
 // testes()
 
 function carregarcatalogo(){
-    for (let i = 0; i < 4;i++){
+    var livros = [
+        ['images/livros/operegrino.jpg','O peregrino'],
+        ['images/livros/OSenhorDosAneis.jpg','O Senhor dos Anéis'],
+        ['images/livros/OHobit.jpg','O Hobbit'],
+        ['images/livros/AsCronicasDeNarnia.jpg','As Crônicas de Nárnia'],
+        ['images/livros/1984.jpg','1984'],
+        ['images/livros/Paulo.jpg','Paulo'],
+        ['images/livros/Apocrifos.jpg','Apócrifos Vol 1'],
+        ['images/livros/Apiocrifos2.jpg','Apócrifos Vol 2'],
+        ['images/livros/Apocrifos3.jpeg','Apócrifos Vol 3']
+
+    ]
+
+    livros.forEach(cadaLivro =>{
         document.getElementById('catalogo').innerHTML += `
-        <div class="livro">
+            <div class="livro">
+    
+                   <img src="${cadaLivro[0]}" alt="">
+                  <h4>${cadaLivro[1]}</h4>
+    
+                  <button>
+                        Adicionar
+                        <img src="images/icons/plus.svg" alt="">
+                   </button>
+                </div>
+    
+             `
+    })
 
-                <img src="images/livros/operegrino.jpg" alt="">
-                <h4>O peregrino</h4>
 
-                <button>
-                    Adicionar
-                    <img src="images/icons/plus.svg" alt="">
-                </button>
-            </div>
+    // livros (let i = 0; i < 4;i++){
+    //     document.getElementById('catalogo').innerHTML += `
+    //     <div class="livro">
 
-        `
-    }
+    //             <img src="" alt="">
+    //             <h4></h4>
+
+    //             <button>
+    //                 Adicionar
+    //                 <img src="images/icons/plus.svg" alt="">
+    //             </button>
+    //         </div>
+
+    //     `
+    // }
 }
 
 carregarcatalogo()
