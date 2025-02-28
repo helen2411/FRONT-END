57690b2c8f1f72195fe8a52a7e5b32c049707fee
helen2411 <helen.l.gomes@aluno.senai.br>
2025-02-28 14:23:28 -0300

    Atualização

---
+++ Back_End/livraria/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livraria</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="pagina.css">
+</head>
+<body>
+
+<header>
+        <div id="logo">
+
+            <h2>Livraria</h2>
+
+        </div>
+        <nav>
+            <a href="#">Home</a>
+        </nav>
+    
+    </header>
+
+    <main>
+        <h1>Livros</h1>
+    </main>
+
+    <div class="card" style="width: 18rem;">
+    <img src="livro_peregrino.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">O Peregrino</h5>
+        <p class="card-text">O Peregrino relata a caminhada de um personagem identificado simplesmente como Cristão, um homem espiritualmente abatido que sai de sua casa em busca da salvação na Cidade Celestial.O caminho é realizado em etapas em que ele encontra vários personagens, que não são pessoas, mas alegorias para as tribulações, sofrimentos e lutas intensas que permeiam a vida dos crentes.</p>
+        <a href="#" class="btn btn-primary">Comprar</a>
+    </div>
+    </div>
+
+
+    <script></script>
+</body>
+</html>
