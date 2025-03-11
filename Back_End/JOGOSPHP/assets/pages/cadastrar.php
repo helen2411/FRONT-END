04ea04016b266d08f7238790e8c41e8ea4e70a45
helen2411 <helen.l.gomes@aluno.senai.br>
2025-03-11 17:43:04 -0300

    Atualização
    Atualização Jogos PHP

---
+++ Back_End/JOGOSPHP/assets/pages/cadastrar.php
@@ -0,0 +1,17 @@
+<?php
+    include "../components/base.html.php";
+?>
+
+<main>
+    <form action="../scripts/cadastro.php" method="post">
+        <input type="text" id="nome" name="nome">
+        <input type="email" id="email" name="email">
+        <input type="password" id="senha" name="senha">
+        <button type="submit">cadastro</button>
+    </form>
+</main>
+
+
+<?php
+    include "../components/baseScriptsHtml.php";
+?>
--- Back_End/JOGOSPHP/index.php
+++ Back_End/JOGOSPHP/index.php
@@ -7,7 +7,27 @@
 </head>
 <body>
 
-    <a href="assets/pages/home.php">Home</a>
+   <main>
+        <form action="assets/scripts/login.php" method="post">
+
+            <!-- email -->
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input placeholder="Digite um email 'jhondoe@gmail.com'" type="email" require name="email" id="email">
+                </div>
+
+            <!-- senha -->
+                <div class="form-group">
+                    <label for="senha">Senha:</label>
+                    <input placeholder="Digite a sua senha" type="password" require name="senha" id="senha">
+                </div>
+            <button type="submit">Login</button>
+            <p>Esqueceu a senha ? <a href="#">Clique aqui</a></p>
+            <p>Não possui conta ? <a href="assets/pages/cadastrar.php">Cadastre-se</a></p>
+
+
+        </form>
+   </main>
     
 </body>
 </html>
