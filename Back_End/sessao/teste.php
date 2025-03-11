2e8b469c41a021f4acd5c4b1d47cbe84827fb2fa
helen2411 <helen.l.gomes@aluno.senai.br>
2025-03-11 16:31:31 -0300

    Atualização
    Aula 11/03

---
+++ Back_End/sessao/teste.php
@@ -0,0 +1,10 @@
+<?php
+
+    session_start();
+    echo $_SESSION['nome'];
+    echo " comprou uma ";
+    //echo $_SESSION['produto']
+    var_dump($_SESSION['produto']) ;
+    session_destroy();
+
+?>
