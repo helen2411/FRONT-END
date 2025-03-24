69ee24b7f0a0ddf7287323ff0676ed09673caccc
helen2411 <helen.l.gomes@aluno.senai.br>
2025-03-24 17:36:26 -0300

    Carros
    Aula dia 24/03/2025

---
+++ Back_End/Carro/index.php
@@ -0,0 +1,45 @@
+<?php 
+    include('db/conexao.php')
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Concessionária </title>
+    <link rel="stylesheet" href="style/index.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="index.php?menu=home">Home</a>
+            <a href="index.php?menu=lista">Lista</a>
+        </nav>
+    </header>
+    
+    <main>
+        <?php
+        if(isset($_GET['menu'])){
+            $pagina = $_GET['menu'];
+        }else{
+            $pagina = "home";
+        }    
+
+       switch($pagina){
+            case 'home':
+                include("pages/Home/home.php");
+                break;
+            case 'lista':
+                include("pages/listaCarros/listaCarros.php");
+                break;
+            default:
+                include("pages/Home/home.php");
+                break;
+         }
+               
+        ?>
+    </main>
+</body>
+</html>
---
+++ Back_End/Carro/pages/listaCarros/listaCarros.php
@@ -0,0 +1,31 @@
+<h1>Lista de Carros</h1>
+
+<table>
+    <tr>
+        <th>Modelo</th>
+        <th>Marca</th>
+        <th>Valor</th>
+        <th>Ano</th>
+        <th>Cor</th>
+    </tr>
+    <?php
+        $sql ="SELECT * FROM carros";
+        // pedido
+        $query = mysqli_query($conexao,$sql) or die("Erro na requisição!".mysqli_error($conexao));
+
+        //fetch_asso = vai acessar um query, e contar os resultados
+        while($dados = mysqli_fetch_assoc($query)){
+            ?>
+                <tr>
+                    <td><?= $dados['modeloCarro'] ?></td>
+                    <td><?= $dados['marcaCarro'] ?></td>
+                    <td><?= $dados['valorCarro'] ?></td>
+                    <td><?= $dados['anoCarro'] ?></td>
+                    <td><?= $dados['corCarro'] ?></td>
+                </tr>
+            <?php
+        }
+
+    ?>
+
+</table>
