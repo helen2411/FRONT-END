9d4dea8982bbd038a95990630406e5272a9cc83c
helen2411 <helen.l.gomes@aluno.senai.br>
2025-03-25 17:28:09 -0300

    Atividade

---
+++ Back_End/comida/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cardápio</title>
+    <style>
+        /* Estilos básicos para o cardápio */
+        body {
+            font-family: sans-serif;
+        }
+        .item-cardapio {
+            display: inline-block;
+            margin: 10px;
+            text-align: center;
+        }
+        .item-cardapio img {
+            width: 200px;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <h1>Cardápio</h1>
+
+    <?php
+     
+    
+    ?>
+
+</body>
+</html>
